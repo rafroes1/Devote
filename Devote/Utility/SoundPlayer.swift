8d16e8f5dae9ed33a789f85a3d1130c7eebc0d01
rafroes1 <rafafroes@outlook.com>
2023-02-28 17:01:11 -0300

    Added sound and haptics ux notifications

---
+++ Devote/Utility/SoundPlayer.swift
@@ -0,0 +1,22 @@
+//
+//  SoundPlayer.swift
+//  Devote
+//
+//  Created by Rafael Carvalho on 28/02/23.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer =  try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Could not find and play the sound file.")
+        }
+    }
+}
